858483ea1c4573d41214ec686f3f4462bce508d7
Lucas POUZAC <lucas.pouzac.pro@gmail.com>
2014-03-26 14:56:58 +0100

    Add Integration Test

---
+++ src/test/java/org/jmeter/reporting/rest/SampleResourceSpecTest.java
@@ -0,0 +1,23 @@
+package org.jmeter.reporting.rest;
+
+import org.junit.runner.RunWith;
+
+import restx.tests.FindSpecsIn;
+import restx.tests.RestxSpecTestsRunner;
+
+@RunWith(RestxSpecTestsRunner.class)
+@FindSpecsIn("specs/sample")
+public class SampleResourceSpecTest {
+
+    /**
+     * Useless, thanks to both @RunWith(RestxSpecTestsRunner.class) & @FindSpecsIn()
+     *
+     * @Rule
+     * public RestxSpecRule rule = new RestxSpecRule();
+     *
+     * @Test
+     * public void test_spec() throws Exception {
+     *     rule.runTest(specTestPath);
+     * }
+     */
+}
